c9ca849ebdb80925607f694a3150e6ab974321af
ruanbrito3 <ruan.brito9@aluno.ce.gov.br>
2023-10-30 14:05:49 -0300

    first commit

---
+++ atividade23.py
@@ -0,0 +1,9 @@
+# Exercício Python 23: Faça um programa que calcule a soma entre todos os números que são múltiplos de três
+# e que se encontram no intervalo de 1 até 500.
+soma = 0
+for i in range(1,501):
+     if i % 3 == 0:
+        print(i)
+        soma += i
+
+print(f"A soma é: {soma}")
